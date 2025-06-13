1bd36899ae6170ff8ed32325efcb73fef6dffb6d
Yokesh-Dev <yokeshramana21@gmail.com>
2025-06-13 13:23:05 +0530

    animated-counter-updated

--- portfolio/src/constants/index.js
+++ portfolio/src/constants/index.js
@@ -36,10 +36,10 @@ const navLinks = [
   ];
   
   const counterItems = [
-    { value: 15, suffix: "+", label: "Years of Experience" },
-    { value: 200, suffix: "+", label: "Satisfied Clients" },
-    { value: 108, suffix: "+", label: "Completed Projects" },
-    { value: 90, suffix: "%", label: "Client Retention Rate" },
+    { value: 4, suffix: "+", label: "Years of Experience" },
+    { value: 10, suffix: "+", label: "Completed Projects" },
+    { value: 60, suffix: "+", label: "Commits Pushed" },
+    { value: 20, suffix: "+", label: "Certificates Earned" },
   ];
   
   const logoIconsList = [
