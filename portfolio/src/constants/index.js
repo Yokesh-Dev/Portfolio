66a44c7591f8b6a8e9bc532b1a9a8a96e31eabe4
Yokesh-Dev <yokeshramana21@gmail.com>
2025-06-14 00:28:17 +0530

    Client-logo-update

--- portfolio/src/constants/index.js
+++ portfolio/src/constants/index.js
@@ -44,38 +44,15 @@ const navLinks = [
   
   const logoIconsList = [
     {
-      imgPath: "/images/logos/company-logo-1.png",
+      imgPath: "/images/L1.png",
     },
     {
-      imgPath: "/images/logos/company-logo-2.png",
+      imgPath: "/images/L2.png",
     },
     {
-      imgPath: "/images/logos/company-logo-3.png",
+      imgPath: "/images/L3.png",
     },
-    {
-      imgPath: "/images/logos/company-logo-4.png",
-    },
-    {
-      imgPath: "/images/logos/company-logo-5.png",
-    },
-    {
-      imgPath: "/images/logos/company-logo-6.png",
-    },
-    {
-      imgPath: "/images/logos/company-logo-7.png",
-    },
-    {
-      imgPath: "/images/logos/company-logo-8.png",
-    },
-    {
-      imgPath: "/images/logos/company-logo-9.png",
-    },
-    {
-      imgPath: "/images/logos/company-logo-10.png",
-    },
-    {
-      imgPath: "/images/logos/company-logo-11.png",
-    },
+
   ];
   
   const abilities = [
