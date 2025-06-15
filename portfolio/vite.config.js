2f20d3c42341f4377309d74ecee192d19443c136
Yokesh-Dev <yokeshramana21@gmail.com>
2025-06-15 22:47:17 +0530

    issue

--- portfolio/vite.config.js
+++ portfolio/vite.config.js
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 export default defineConfig({
-  plugins: [
+ 
+  plugins: 
+  [
+    
     tailwindcss(),react()
+    
   ],
-})
+  base: process.env.VITE_BASE_PATH || "/Portfolio",
+  
+})    
